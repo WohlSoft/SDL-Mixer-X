3a56d3eed155b99bd96f57bb03748576dddd8584
Vitaly Novichkov <admin@wohlnet.ru>
2018-01-05 21:19:45 +0300

    Add example Qt application that widely uses SDL Mixer X to easier debug it

---
+++ examples/MusPlay-Qt/MainWindow/musplayer_base.cpp
@@ -0,0 +1,83 @@
+#include "musplayer_base.h"
+#include "../Player/mus_player.h"
+
+MusPlayerBase::MusPlayerBase() :
+    m_blink_state(false),
+    m_prevTrackID(0)
+{}
+
+MusPlayerBase::~MusPlayerBase()
+{}
+
+void MusPlayerBase::openMusicByArg(QString)
+{}
+
+void MusPlayerBase::on_open_clicked()
+{}
+
+void MusPlayerBase::on_stop_clicked()
+{}
+
+void MusPlayerBase::on_play_clicked()
+{}
+
+void MusPlayerBase::on_volume_valueChanged(int value)
+{
+    PGE_MusicPlayer::MUS_changeVolume(value);
+}
+
+void MusPlayerBase::on_mididevice_currentIndexChanged(int) {}
+
+void MusPlayerBase::on_fmbank_currentIndexChanged(int index)
+{
+    Mix_ADLMIDI_setBankID(index);
+
+    if(Mix_PlayingMusic())
+    {
+        if(PGE_MusicPlayer::type == MUS_MID)
+        {
+            PGE_MusicPlayer::MUS_stopMusic();
+            on_play_clicked();
+        }
+    }
+}
+
+void MusPlayerBase::on_volumeModel_currentIndexChanged(int index)
+{
+    Mix_ADLMIDI_setVolumeModel(index);
+}
+
+void MusPlayerBase::on_tremolo_clicked(bool checked)
+{
+    Mix_ADLMIDI_setTremolo((int)checked);
+}
+
+void MusPlayerBase::on_vibrato_clicked(bool checked)
+{
+    Mix_ADLMIDI_setVibrato((int)checked);
+}
+
+void MusPlayerBase::on_modulation_clicked(bool checked)
+{
+    Mix_ADLMIDI_setScaleMod((int)checked);
+}
+
+void MusPlayerBase::on_adlibMode_clicked(bool checked)
+{
+    Mix_ADLMIDI_setAdLibMode((int)checked);
+}
+
+void MusPlayerBase::on_logVolumes_clicked(bool checked)
+{
+    Mix_ADLMIDI_setLogarithmicVolumes(checked);
+}
+
+void MusPlayerBase::on_trackID_editingFinished() {}
+
+void MusPlayerBase::on_recordWav_clicked(bool) {}
+
+void MusPlayerBase::_blink_red()
+{}
+
+void MusPlayerBase::on_resetDefaultADLMIDI_clicked()
+{}
