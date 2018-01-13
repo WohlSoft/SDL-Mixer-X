0c5d5f57c59a87924202f87bcb15190db5121650
Vitaly Novichkov <admin@wohlnet.ru>
2018-01-13 21:29:22 +0300

    WAV: warning fixes

--- src/codecs/music_wav.c
+++ src/codecs/music_wav.c
@@ -163,7 +163,7 @@ static void *WAV_CreateFromRW(SDL_RWops *src, int freesrc)
         return NULL;
     }
 
-    music->freesrc = freesrc;
+    music->freesrc = (SDL_bool)freesrc;
     return music;
 }
 
@@ -196,8 +196,8 @@ static int WAV_GetSome(void *context, void *data, int bytes, SDL_bool *done)
     WAV_Music *music = (WAV_Music *)context;
     Sint64 pos, stop;
     WAVLoopPoint *loop;
-    Sint64 loop_start;
-    Sint64 loop_stop;
+    Sint64 loop_start = music->start;
+    Sint64 loop_stop = music->stop;
     SDL_bool looped = SDL_FALSE;
     int i;
     int filled, amount, result;
@@ -220,8 +220,8 @@ static int WAV_GetSome(void *context, void *data, int bytes, SDL_bool *done)
         loop = &music->loops[i];
         if (loop->active) {
             const int bytes_per_sample = (SDL_AUDIO_BITSIZE(music->spec.format) / 8) * music->spec.channels;
-            loop_start = music->start + loop->start * bytes_per_sample;
-            loop_stop = music->start + (loop->stop + 1) * bytes_per_sample;
+            loop_start = music->start + loop->start * (Uint32)bytes_per_sample;
+            loop_stop = music->start + (loop->stop + 1) * (Uint32)bytes_per_sample;
             if (pos >= loop_start && pos < loop_stop)
             {
                 stop = loop_stop;
@@ -231,11 +231,11 @@ static int WAV_GetSome(void *context, void *data, int bytes, SDL_bool *done)
         loop = NULL;
     }
 
-    amount = music->spec.size;
+    amount = (int)music->spec.size;
     if ((stop - pos) < amount) {
         amount = (int)(stop - pos);
     }
-    amount = (int)SDL_RWread(music->src, music->buffer, 1, amount);
+    amount = (int)SDL_RWread(music->src, music->buffer, 1, (size_t)amount);
     if (amount > 0) {
         result = SDL_AudioStreamPut(music->stream, music->buffer, amount);
         if (result < 0) {
@@ -252,7 +252,7 @@ static int WAV_GetSome(void *context, void *data, int bytes, SDL_bool *done)
             if (loop->current_play_count > 0) {
                 --loop->current_play_count;
             }
-            SDL_RWseek(music->src, loop_start, RW_SEEK_SET);
+            SDL_RWseek(music->src, (Sint64)loop_start, RW_SEEK_SET);
             looped = SDL_TRUE;
         }
     }
@@ -335,7 +335,7 @@ static SDL_bool ParseFMT(WAV_Music *wave, Uint32 chunk_length)
             Mix_SetError("Unknown WAVE data format");
             goto done;
     }
-    spec->freq = SDL_SwapLE32(format->frequency);
+    spec->freq = (int)SDL_SwapLE32(format->frequency);
     switch (SDL_SwapLE16(format->bitspersample)) {
         case 8:
             spec->format = AUDIO_U8;
@@ -372,7 +372,7 @@ static SDL_bool ParseDATA(WAV_Music *wave, Uint32 chunk_length)
 static SDL_bool AddLoopPoint(WAV_Music *wave, Uint32 play_count, Uint32 start, Uint32 stop)
 {
     WAVLoopPoint *loop;
-    WAVLoopPoint *loops = SDL_realloc(wave->loops, (wave->numloops + 1)*sizeof(*wave->loops));
+    WAVLoopPoint *loops = SDL_realloc(wave->loops, (size_t)(wave->numloops + 1) * sizeof(*wave->loops));
     if (!loops) {
         Mix_SetError("Out of memory");
         return SDL_FALSE;
@@ -501,8 +501,8 @@ static Uint32 SANE_to_Uint32 (Uint8 *sanebuf)
     if (sanebuf[0] == 0x40 && sanebuf[1] > 0x1C)
         return 800000000;
 
-    return ((sanebuf[2] << 23) | (sanebuf[3] << 15) | (sanebuf[4] << 7) |
-            (sanebuf[5] >> 1)) >> (29 - sanebuf[1]);
+    return (Uint32)(((sanebuf[2] << 23) | (sanebuf[3] << 15) | (sanebuf[4] << 7) |
+            (sanebuf[5] >> 1)) >> (29 - sanebuf[1]));
 }
 
 static SDL_bool LoadAIFFMusic(WAV_Music *wave)
@@ -590,7 +590,7 @@ static SDL_bool LoadAIFFMusic(WAV_Music *wave)
 
     /* Decode the audio data format */
     SDL_memset(spec, 0, (sizeof *spec));
-    spec->freq = frequency;
+    spec->freq = (int)frequency;
     switch (samplesize) {
         case 8:
             spec->format = AUDIO_S8;
