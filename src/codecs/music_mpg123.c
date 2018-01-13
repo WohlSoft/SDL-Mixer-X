66fe0641d5968c504ee8512c0b00b327a9229bcb
Vitaly Novichkov <admin@wohlnet.ru>
2018-01-13 13:58:00 +0300

    MPG123: Implemented full seekability and ID3 tags

--- src/codecs/music_mpg123.c
+++ src/codecs/music_mpg123.c
@@ -29,6 +29,7 @@
 #include "SDL_loadso.h"
 
 #include "music_mpg123.h"
+#include "music_id3tag.h"
 
 #include <mpg123.h>
 
@@ -51,6 +52,8 @@ typedef struct {
     int (*mpg123_read)(mpg123_handle *mh, unsigned char *outmemory, size_t outmemsize, size_t *done );
     int (*mpg123_replace_reader_handle)( mpg123_handle *mh, ssize_t (*r_read) (void *, void *, size_t), off_t (*r_lseek)(void *, off_t, int), void (*cleanup)(void*) );
     off_t (*mpg123_seek)( mpg123_handle *mh, off_t sampleoff, int whence );
+    off_t (*mpg123_tell)( mpg123_handle *mh);
+    off_t (*mpg123_length)(mpg123_handle *mh);
     const char* (*mpg123_strerror)(mpg123_handle *mh);
 } mpg123_loader;
 
@@ -98,6 +101,8 @@ static int MPG123_Load(void)
         FUNCTION_LOADER(mpg123_read, int (*)(mpg123_handle *mh, unsigned char *outmemory, size_t outmemsize, size_t *done ))
         FUNCTION_LOADER(mpg123_replace_reader_handle, int (*)( mpg123_handle *mh, ssize_t (*r_read) (void *, void *, size_t), off_t (*r_lseek)(void *, off_t, int), void (*cleanup)(void*) ))
         FUNCTION_LOADER(mpg123_seek, off_t (*)( mpg123_handle *mh, off_t sampleoff, int whence ))
+        FUNCTION_LOADER(mpg123_tell, off_t (*)( mpg123_handle *mh))
+        FUNCTION_LOADER(mpg123_length, off_t (*)( mpg123_handle *mh))
         FUNCTION_LOADER(mpg123_strerror, const char* (*)(mpg123_handle *mh))
     }
     ++mpg123.loaded;
@@ -130,6 +135,9 @@ typedef struct
     SDL_AudioStream *stream;
     unsigned char *buffer;
     size_t buffer_size;
+    off_t total_length;
+    long sample_rate;
+    Mix_MusicMetaTags tags;
 } MPG123_Music;
 
 
@@ -200,6 +208,7 @@ static void rwops_cleanup(void* p)
 
 static int MPG123_Open(const SDL_AudioSpec *spec)
 {
+    (void)spec;
     if (mpg123.mpg123_init() != MPG123_OK) {
         Mix_SetError("mpg123_init() failed");
         return -1;
@@ -210,7 +219,8 @@ static int MPG123_Open(const SDL_AudioSpec *spec)
 static void *MPG123_CreateFromRW(SDL_RWops *src, int freesrc)
 {
     MPG123_Music *music;
-    int result;
+    int result, format, channels, encoding;
+    long rate;
     const long *rates;
     size_t i, num_rates;
 
@@ -220,6 +230,8 @@ static void *MPG123_CreateFromRW(SDL_RWops *src, int freesrc)
     }
     music->src = src;
     music->volume = MIX_MAX_VOLUME;
+    meta_tags_init(&music->tags);
+    id3tag_fetchTags(&music->tags, music->src);
 
     /* Just assume 16-bit 2 channel audio for now */
     music->buffer_size = music_spec.samples * sizeof(Sint16) * 2;
@@ -271,6 +283,32 @@ static void *MPG123_CreateFromRW(SDL_RWops *src, int freesrc)
     if (result != MPG123_OK) {
         MPG123_Delete(music);
         Mix_SetError("mpg123_open_handle: %s", mpg_err(music->handle, result));
+        return NULL;
+    }
+
+    music->total_length = mpg123.mpg123_length(music->handle);
+    if (music->total_length < 0) {
+        MPG123_Delete(music);
+        Mix_SetError("mpg123_length: %s", mpg_err(music->handle, (int)-music->total_length));
+        return NULL;
+    }
+
+    result = mpg123.mpg123_getformat(music->handle, &rate, &channels, &encoding);
+    if (result != MPG123_OK) {
+        MPG123_Delete(music);
+        Mix_SetError("mpg123_getformat: %s", mpg_err(music->handle, result));
+        return NULL;
+    }
+/*printf("MPG123 format: %s, channels = %d, rate = %ld\n", mpg123_format_str(encoding), channels, rate);*/
+
+    format = mpg123_format_to_sdl(encoding);
+    SDL_assert(format != -1);
+    music->sample_rate = rate;
+
+    music->stream = SDL_NewAudioStream((SDL_AudioFormat)format, (Uint8)channels, (int)rate,
+                                       music_spec.format, music_spec.channels, music_spec.freq);
+    if (!music->stream) {
+        MPG123_Delete(music);
         return NULL;
     }
 
@@ -331,8 +369,9 @@ static int MPG123_GetSome(void *context, void *data, int bytes, SDL_bool *done)
 
         format = mpg123_format_to_sdl(encoding);
         SDL_assert(format != -1);
+        music->sample_rate = rate;
 
-        music->stream = SDL_NewAudioStream(format, channels, (int)rate,
+        music->stream = SDL_NewAudioStream((SDL_AudioFormat)format, (Uint8)channels, (int)rate,
                                            music_spec.format, music_spec.channels, music_spec.freq);
         if (!music->stream) {
             return -1;
@@ -376,10 +415,41 @@ static int MPG123_Seek(void *context, double secs)
     return 0;
 }
 
+static double MPG123_Tell(void *context)
+{
+    MPG123_Music *music = (MPG123_Music *)context;
+    off_t offset = 0;
+    if (!music->sample_rate) {
+        return 0.0;
+    }
+    if ((offset = mpg123.mpg123_tell(music->handle)) < 0) {
+        return Mix_SetError("mpg123_tell: %s", mpg_err(music->handle, (int)-offset));
+    }
+    return (double)offset / (double)music->sample_rate;
+}
+
+
+static double MPG123_Length(void *context)
+{
+    MPG123_Music *music = (MPG123_Music *)context;
+    if (!music->sample_rate) {
+        return -1.0;
+    }
+    return (double)music->total_length / (double)music->sample_rate;
+}
+
+
+static const char* MPG123_GetMetaTag(void *context, Mix_MusicMetaTag tag_type)
+{
+    MPG123_Music *music = (MPG123_Music *)context;
+    return meta_tags_get(&music->tags, tag_type);
+}
+
 static void MPG123_Delete(void *context)
 {
     MPG123_Music *music = (MPG123_Music *)context;
 
+    meta_tags_clear(&music->tags);
     if (music->handle) {
         mpg123.mpg123_close(music->handle);
         mpg123.mpg123_delete(music->handle);
@@ -420,12 +490,12 @@ Mix_MusicInterface Mix_MusicInterface_MPG123 =
     NULL,   /* IsPlaying */
     MPG123_GetAudio,
     MPG123_Seek,
-    NULL,   /* Tell [MIXER-X]*/
-    NULL,   /* FullLength [MIXER-X]*/
+    MPG123_Tell, /* Tell [MIXER-X]*/
+    MPG123_Length, /* FullLength [MIXER-X]*/
     NULL,   /* LoopStart [MIXER-X]*/
     NULL,   /* LoopEnd [MIXER-X]*/
     NULL,   /* LoopLength [MIXER-X]*/
-    NULL,   /* GetMetaTag [MIXER-X]*/
+    MPG123_GetMetaTag,/* GetMetaTag [MIXER-X]*/
     NULL,   /* Pause */
     NULL,   /* Resume */
     NULL,   /* Stop */
