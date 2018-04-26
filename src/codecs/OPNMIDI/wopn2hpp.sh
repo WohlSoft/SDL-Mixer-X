1629f6538ab305d1ea6285d482f46eeeac828e7a
Vitaly Novichkov <admin@wohlnet.ru>
2018-04-27 02:09:57 +0300

    Update default bank for OPNMIDI

--- src/codecs/OPNMIDI/wopn2hpp.sh
+++ src/codecs/OPNMIDI/wopn2hpp.sh
@@ -16,7 +16,7 @@ out "   and then run a wopn2hpp.sh script to generate this file again\n"
 out " *===============================================================*/\n\n"
 out "static unsigned char g_gm_opn2_bank[] = \n"
 out "{\n"
-hexdump -ve '12/1 "0x%02x, " "\n"' gm.wopn >> $output
+hexdump -ve '12/1 "0x%02x, " "\n"' xg.wopn >> $output
 out "0x00\n"
 out "};\n"
 out "\n"
