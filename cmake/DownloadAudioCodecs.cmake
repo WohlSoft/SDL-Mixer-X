bcad2b5f4e75bf04db9328618824d8150041b395
Wohlstand <admin@wohlnet.ru>
2019-07-22 12:05:44 +0300

    AppVeyor: Produce separate build with SDL-2.0.9 usage

--- cmake/DownloadAudioCodecs.cmake
+++ cmake/DownloadAudioCodecs.cmake
@@ -17,9 +17,22 @@ find_package(Git REQUIRED)
 set(AUDIO_CODECS_INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR})
 set(AUDIO_CODECS_REPOSITORY_PATH ${CMAKE_BINARY_DIR}/external/AudioCodecs)
 
+#set(AUDIO_CODECS_REPO_PATH "" CACHE PATH "Path to the AudioCodecs dependencies pack")
+
+set(AUDIO_CODECS_SDL2_HG_BRANCH "" CACHE PATH "HG branch for SDL2 (official Mercurial mainstream repo)")
+set(AUDIO_CODECS_SDL2_GIT_BRANCH "" CACHE PATH "GIT branch for SDL2 (unofficial Git mirror)")
+
 option(WITH_SDL2_WASAPI "Enable WASAPI audio output support for Windows build of SDL2" ON)
 if(WIN32)
     set(SDL2_WASAPI_FLAG "-DSDL2_WASAPI_FLAG=${WITH_SDL2_WASAPI}")
+endif()
+
+set(SDL2_TAGS)
+if(AUDIO_CODECS_SDL2_HG_BRANCH)
+    set(SDL2_TAGS ${SDL2_TAGS} "-DSDL2_HG_BRANCH=${AUDIO_CODECS_SDL2_HG_BRANCH}")
+endif()
+if(AUDIO_CODECS_SDL2_GIT_BRANCH)
+    set(SDL2_TAGS ${SDL2_TAGS} "-DSDL2_GIT_BRANCH=${AUDIO_CODECS_SDL2_GIT_BRANCH}")
 endif()
 
 ExternalProject_Add(
@@ -31,6 +44,7 @@ ExternalProject_Add(
         "-DCMAKE_INSTALL_PREFIX=${AUDIO_CODECS_INSTALL_DIR}"
         "-DDOWNLOAD_SDL2_DEPENDENCY=ON"
         ${SDL2_WASAPI_FLAG}
+        ${SDL2_TAGS}
 )
 
 message("AudioCodecs can see SDL2 is stored in ${SDL2_REPO_PATH}...")
