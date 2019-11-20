a7a3049b9311a4fdd0f3d106d5f88cbb65e376e3
Vitaly Novichkov <admin@wohlnet.ru>
2019-11-21 00:47:37 +0300

    Attempt to fix Opus build

--- src/codecs/music_opus.cmake
+++ src/codecs/music_opus.cmake
@@ -3,23 +3,23 @@ if(USE_OPUS)
     option(USE_OPUS_DYNAMIC "Use dynamical loading of Opus" OFF)
 
     if(USE_SYSTEM_AUDIO_LIBRARIES)
-        find_package(Opus)
+        find_package(Opus REQUIRED)
         message("Opus: [${Opus_FOUND}] ${Opus_INCLUDE_DIRS} ${Opus_LIBRARIES} ${LIBOPUS_LIB}")
         if(USE_OPUS_DYNAMIC)
             add_definitions(-DOPUS_DYNAMIC=\"${OpusFile_DYNAMIC_LIBRARY}\")
             message("Dynamic Opus: ${OpusFile_DYNAMIC_LIBRARY}")
         endif()
     else()
-        set(Opus_FOUND 1)
         if(DOWNLOAD_AUDIO_CODECS_DEPENDENCY)
-            set(Opus_LIBRARIES opusfile)
-            set(LIBOPUS_LIB opus)
+            set(Opus_LIBRARIES opusfile opus)
         else()
-            find_library(Opus_LIBRARIES NAMES opusfile
+            find_library(LIBOPUSFILE_LIB NAMES opusfile
                          HINTS "${AUDIO_CODECS_INSTALL_PATH}/lib")
             find_library(LIBOPUS_LIB NAMES opus
                          HINTS "${AUDIO_CODECS_INSTALL_PATH}/lib")
+            set(Opus_LIBRARIES ${LIBOPUSFILE_LIB} ${LIBOPUS_LIB})
         endif()
+        set(Opus_FOUND 1)
         set(Opus_INCLUDE_DIRS
             ${AUDIO_CODECS_PATH}/libogg/include
             ${AUDIO_CODECS_PATH}/libopus/include
