1d6d2596a01b10d42b11cc1fb5b1cecd63aea51f
Vitaly Novichkov <admin@wohlnet.ru>
2019-11-24 14:39:12 +0300

    Make dynamic FLAC work

--- src/codecs/music_flac.cmake
+++ src/codecs/music_flac.cmake
@@ -1,36 +1,39 @@
 option(USE_FLAC            "Build with FLAC codec" ON)
 if(USE_FLAC)
+    option(USE_FLAC_DYNAMIC "Use dynamical loading of FLAC" OFF)
+
     if(USE_SYSTEM_AUDIO_LIBRARIES)
-        find_path(LIBFLAC_INCLUDE_DIR "FLAC/all.h")
-        find_library(LIBFLAC_LIB NAMES FLAC)
-        if(LIBFLAC_INCLUDE_DIR AND LIBFLAC_LIB)
-            set(FOUND_FLAC 1)
+        find_package(FLAC REQUIRED)
+        message("FLAC: [${FOUND_FLAC}] ${FLAC_INCLUDE_DIRS} ${FLAC_LIBRARIES}")
+        if(USE_FLAC_DYNAMIC)
+            add_definitions(-DFLAC_DYNAMIC=\"${FLAC_DYNAMIC_LIBRARY}\")
+            message("Dynamic FLAC: ${FLAC_DYNAMIC_LIBRARY}")
         endif()
-        message("FLAC: [${FOUND_FLAC}] ${LIBFLAC_INCLUDE_DIR} ${LIBFLAC_LIB}")
     else()
-        set(FOUND_FLAC 1)
         if(DOWNLOAD_AUDIO_CODECS_DEPENDENCY)
-            set(LIBFLAC_LIB FLAC)
+            set(FLAC_LIBRARIES FLAC)
         else()
-            find_library(LIBFLAC_LIB NAMES FLAC
+            find_library(FLAC_LIBRARIES NAMES FLAC
                          HINTS "${AUDIO_CODECS_INSTALL_PATH}/lib")
         endif()
+        set(FLAC_FOUND 1)
+        set(FLAC_INCLUDE_DIRS
+            ${AUDIO_CODECS_INSTALL_DIR}/include/FLAC
+            ${AUDIO_CODECS_INSTALL_DIR}/include/ogg
+            ${AUDIO_CODECS_PATH}/libogg/include
+            ${AUDIO_CODECS_PATH}/libFLAC/include
+        )
     endif()
-    if(FOUND_FLAC)
+
+    if(FLAC_FOUND)
         message("== using FLAC ==")
         add_definitions(-DMUSIC_FLAC -DFLAC__NO_DLL)
-        if(AUDIO_CODECS_REPO_PATH)
-            list(APPEND SDL_MIXER_INCLUDE_PATHS
-                ${AUDIO_CODECS_INSTALL_DIR}/include/FLAC
-                ${AUDIO_CODECS_INSTALL_DIR}/include/ogg
-                ${AUDIO_CODECS_PATH}/libogg/include
-                ${AUDIO_CODECS_PATH}/libFLAC/include
-            )
+        if(NOT USE_SYSTEM_AUDIO_LIBRARIES OR NOT USE_FLAC_DYNAMIC)
+            set(LIBOGG_NEEDED ON)
+            list(APPEND SDLMixerX_LINK_LIBS ${FLAC_LIBRARIES})
         endif()
-        set(LIBOGG_NEEDED ON)
-        list(APPEND SDLMixerX_LINK_LIBS ${LIBFLAC_LIB})
+        list(APPEND SDL_MIXER_INCLUDE_PATHS ${FLAC_INCLUDE_DIRS})
         list(APPEND SDLMixerX_SOURCES
             ${CMAKE_CURRENT_LIST_DIR}/music_flac.c)
     endif()
 endif()
-
