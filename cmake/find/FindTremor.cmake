90a79c9aa78b70668d1492af030231bb8895647e
Vitaly Novichkov <admin@wohlnet.ru>
2019-11-25 00:50:56 +0300

    Fixed the Tremor support

--- cmake/find/FindTremor.cmake
+++ cmake/find/FindTremor.cmake
@@ -7,15 +7,15 @@
 find_path(Tremor_INCLUDE_DIR "ivorbisfile.h" PATH_SUFFIXES tremor)
 find_path(Ogg_INCLUDE_DIR "ogg.h" PATH_SUFFIXES ogg)
 
-find_library(VorbisDec_LIBRARY NAMES vorbisdec)
+find_library(VorbisIDec_LIBRARY NAMES vorbisidec)
 
-if(Tremor_INCLUDE_DIR AND VorbisDec_LIBRARY)
+if(Tremor_INCLUDE_DIR AND VorbisIDec_LIBRARY)
     if(APPLE)
-        find_library(VorbisDec_DYNAMIC_LIBRARY NAMES "vorbisdec"  PATH_SUFFIXES ".dylib")
+        find_library(VorbisIDec_DYNAMIC_LIBRARY NAMES "vorbisidec"  PATH_SUFFIXES ".dylib")
     elseif(WIN32)
-        find_library(VorbisDec_DYNAMIC_LIBRARY NAMES "vorbisdec" PATH_SUFFIXES ".dll")
+        find_library(VorbisIDec_DYNAMIC_LIBRARY NAMES "vorbisidec" PATH_SUFFIXES ".dll")
     else()
-        find_library(VorbisDec_DYNAMIC_LIBRARY NAMES "vorbisdec" PATH_SUFFIXES ".so")
+        find_library(VorbisIDec_DYNAMIC_LIBRARY NAMES "vorbisidec" PATH_SUFFIXES ".so")
     endif()
 endif()
 
@@ -23,9 +23,9 @@ include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set Tremor_FOUND to TRUE
 # if all listed variables are TRUE
 find_package_handle_standard_args(Tremor DEFAULT_MSG
-                                  VorbisDec_LIBRARY Tremor_INCLUDE_DIR)
+                                  VorbisIDec_LIBRARY Tremor_INCLUDE_DIR)
 
-mark_as_advanced(Ogg_INCLUDE_DIR Tremor_INCLUDE_DIR VorbisDec_LIBRARY)
+mark_as_advanced(Ogg_INCLUDE_DIR Tremor_INCLUDE_DIR VorbisIDec_LIBRARY)
 
-set(Tremor_LIBRARIES ${VorbisDec_LIBRARY})
+set(Tremor_LIBRARIES ${VorbisIDec_LIBRARY})
 set(Tremor_INCLUDE_DIRS ${Ogg_INCLUDE_DIR} ${Tremor_INCLUDE_DIR})
