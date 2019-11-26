a2c0d6bdac617444d37ac9e7df499961f7a58bf0
Vitaly Novichkov <admin@wohlnet.ru>
2019-11-26 23:46:55 +0300

    Fixed NoPie check It should use Pure C, not C++

--- cmake/NoPie.cmake
+++ cmake/NoPie.cmake
@@ -1,9 +1,9 @@
-include(CheckCXXCompilerFlag)
+include(CheckCCompilerFlag)
 
 set(CMAKE_POSITION_INDEPENDENT_CODE False)
 
 if(NOT WIN32 AND CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX)
-    check_cxx_compiler_flag("-no-pie" HAS_NO_PIE)
+    check_c_compiler_flag("-no-pie" HAS_NO_PIE)
 endif()
 
 function(pge_set_nopie _target)
