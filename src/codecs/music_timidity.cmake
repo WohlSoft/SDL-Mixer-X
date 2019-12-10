c5541613de84374c75d978a41f57768dd3c040b4
Wohlstand <admin@wohlnet.ru>
2019-12-10 19:00:00 +0300

    Timidity: use "timidity_sdl2" library name

--- src/codecs/music_timidity.cmake
+++ src/codecs/music_timidity.cmake
@@ -3,10 +3,10 @@ if(USE_MIDI_TIMIDITY AND NOT USE_SYSTEM_AUDIO_LIBRARIES)
     message("== using Timidity-SDL ==")
 
     if(DOWNLOAD_AUDIO_CODECS_DEPENDENCY)
-        set(TIMIDITYSDL_LIBRARIES timidity)
+        set(TIMIDITYSDL_LIBRARIES timidity_sdl2)
         set(TIMIDITYSDL_FOUND True)
     else()
-        find_library(TIMIDITYSDL_LIBRARIES NAMES timidity
+        find_library(TIMIDITYSDL_LIBRARIES NAMES timidity_sdl2
                      HINTS "${AUDIO_CODECS_INSTALL_PATH}/lib")
         if(TIMIDITYSDL_LIBRARIES)
             set(TIMIDITYSDL_FOUND True)
