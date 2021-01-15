e83ffc6824f2c7064257406379b814433fdf7659
Wohlstand <admin@wohlnet.ru>
2021-01-15 14:17:07 +0300

    MIDI Sequencer: Disable MUS and XMI when ZLib enforce requested
    #40

--- src/codecs/music_midi_seq.cmake
+++ src/codecs/music_midi_seq.cmake
@@ -4,6 +4,13 @@ if(CPP_MIDI_SEQUENCER_NEEDED)
     list(APPEND SDLMixerX_SOURCES
         ${CMAKE_CURRENT_LIST_DIR}/midi_seq/mix_midi_seq.cpp)
     set(STDCPP_NEEDED TRUE)
-    # LGPL license because of XMI/MUS support modules. Disabling them will give MIT
-    setLicense(LICENSE_LGPL_2_1p)
+    if(SDL_MIXER_CLEAR_FOR_ZLIB_LICENSE)
+        # Disable MUS and XMI formats as they LGPL-licensed
+        message("-- !!! MIDI Sequencer will don't support MUS and XMI formats !!!")
+        list(APPEND SDL_MIXER_DEFINITIONS -DBWMIDI_DISABLE_MUS_SUPPORT -DBWMIDI_DISABLE_XMI_SUPPORT)
+        setLicense(LICENSE_MIT)
+    else()
+        # LGPL license because of XMI/MUS support modules. Disabling them will give MIT
+        setLicense(LICENSE_LGPL_2_1p)
+    endif()
 endif()
