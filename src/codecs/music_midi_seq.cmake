057254ff7613e7fdb7188fd444906b907bed49da
Wohlstand <admin@wohlnet.ru>
2021-03-29 15:08:10 +0300

    Small fix at the CMake build

--- src/codecs/music_midi_seq.cmake
+++ src/codecs/music_midi_seq.cmake
@@ -1,6 +1,5 @@
 # C++-coded alternative MIDI-sequencer
 if(CPP_MIDI_SEQUENCER_NEEDED)
-    list(APPEND SDL_MIXER_DEFINITIONS -DMUSIC_MID_NATIVE -DMUSIC_MID_NATIVE_ALT)
     list(APPEND SDLMixerX_SOURCES
         ${CMAKE_CURRENT_LIST_DIR}/midi_seq/mix_midi_seq.cpp)
     set(STDCPP_NEEDED TRUE)
