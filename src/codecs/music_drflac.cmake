c7c2cbada0bbcec7137a0eaf950c07813e481c76
Wohlstand <admin@wohlnet.ru>
2022-05-23 05:14:56 +0300

    CMake: move dr_flac build code into own file

---
+++ src/codecs/music_drflac.cmake
@@ -0,0 +1,9 @@
+option(USE_DRFLAC "Build with FLAC codec using drflac" ON)
+if(USE_DRFLAC)
+    message("== using FLAC (public domain or MIT-0) ==")
+    list(APPEND SDL_MIXER_DEFINITIONS -DMUSIC_FLAC_DRFLAC)
+    list(APPEND SDLMixerX_SOURCES
+        ${CMAKE_CURRENT_LIST_DIR}/music_drflac.c
+        ${CMAKE_CURRENT_LIST_DIR}/music_drflac.h
+    )
+endif()
--- src/codecs/music_flac.cmake
+++ src/codecs/music_flac.cmake
@@ -42,13 +42,3 @@ if(USE_FLAC)
         message("-- skipping FLAC --")
     endif()
 endif()
-
-option(USE_DRFLAC            "Build with FLAC codec using drflac" ON)
-if(USE_DRFLAC)
-    message("== using FLAC (public domain or MIT-0) ==")
-    list(APPEND SDL_MIXER_DEFINITIONS -DMUSIC_FLAC_DRFLAC)
-    list(APPEND SDLMixerX_SOURCES
-        ${CMAKE_CURRENT_LIST_DIR}/music_drflac.c
-        ${CMAKE_CURRENT_LIST_DIR}/music_drflac.h
-    )
-endif()
