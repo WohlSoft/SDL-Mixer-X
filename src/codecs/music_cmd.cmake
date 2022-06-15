8ccf4326948271ea6839f006a8187bc63938ea88
Wohlstand <admin@wohlnet.ru>
2022-06-15 04:17:33 +0300

    music_cmd.c / CMake: Take some update from mainstream
    Use the different method to detect the fork call, and also detect the presense of vfork too.

--- src/codecs/music_cmd.cmake
+++ src/codecs/music_cmd.cmake
@@ -3,10 +3,28 @@ if(UNIX) # CMD Music is not supported on Windows
     if(USE_CMD)
         message("== using CMD Music (ZLib) ==")
         list(APPEND SDL_MIXER_DEFINITIONS -DMUSIC_CMD -D_POSIX_C_SOURCE=1)
-        CHECK_FUNCTION_EXISTS(fork HAVE_FORK)
-        if(HAVE_FORK)
-            list(APPEND SDL_MIXER_DEFINITIONS -DHAVE_FORK)
+
+        set(fork_found OFF)
+        if(NOT fork_found)
+            check_symbol_exists(fork sys/unistd.h HAVE_FORK)
+            if(HAVE_FORK)
+                set(fork_found ON)
+                list(APPEND SDL_MIXER_DEFINITIONS -DHAVE_FORK)
+            endif()
         endif()
+
+        if(NOT fork_found)
+            check_symbol_exists(fork sys/unistd.h HAVE_VFORK)
+            if(HAVE_VFORK)
+                set(fork_found ON)
+                list(APPEND SDL_MIXER_DEFINITIONS -DHAVE_VFORK)
+            endif()
+        endif()
+
+        if(NOT fork_found)
+            message(FATAL_ERROR "Neither fork() or vfork() or available on this platform. Reconfigure with -DUSE_CMD=OFF.")
+        endif()
+
         list(APPEND SDLMixerX_SOURCES
             ${CMAKE_CURRENT_LIST_DIR}/music_cmd.c
             ${CMAKE_CURRENT_LIST_DIR}/music_cmd.h
