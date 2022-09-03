75ef4ee5add1fae92760af5babb8de63e76873d3
Wohlstand <admin@wohlnet.ru>
2022-09-03 06:14:32 +0300

    CMake: Temporarily use absolute SO paths

--- src/codecs/music_ffmpeg.cmake
+++ src/codecs/music_ffmpeg.cmake
@@ -1,7 +1,12 @@
 option(USE_FFMPEG             "Build with FFMPEG library (for AAC and WMA formats) (LGPL)" OFF)
 if(USE_FFMPEG AND MIXERX_LGPL)
     list(APPEND SDL_MIXER_DEFINITIONS -DMUSIC_FFMPEG)
-    list(APPEND SDLMixerX_LINK_LIBS swresample avcodec avformat avutil)
+    list(APPEND SDLMixerX_LINK_LIBS
+           /usr/local/lib/libswresample.so
+           /usr/local/lib/libavcodec.so
+           /usr/local/lib/libavformat.so
+           /usr/local/lib/libavutil.so
+    )
     list(APPEND SDLMixerX_SOURCES
         ${CMAKE_CURRENT_LIST_DIR}/music_ffmpeg.c
         ${CMAKE_CURRENT_LIST_DIR}/music_ffmpeg.h
