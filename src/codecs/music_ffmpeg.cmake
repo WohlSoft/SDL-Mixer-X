322883a425d14edb95f90944bbb1a4076ee4b3b3
Wohlstand <admin@wohlnet.ru>
2022-09-06 17:52:27 +0300

    CMAke: Tuned the build

--- src/codecs/music_ffmpeg.cmake
+++ src/codecs/music_ffmpeg.cmake
@@ -15,6 +15,12 @@ if(USE_FFMPEG AND MIXERX_LGPL)
             )
             message("Dynamic FFMPEG: ${FFMPEG_avutil_DYNAMIC_LIBRARY} ${FFMPEG_avcodec_DYNAMIC_LIBRARY} ${FFMPEG_avformat_DYNAMIC_LIBRARY} ${FFMPEG_swresample_DYNAMIC_LIBRARY}")
         endif()
+        set(FFMPEG_LINK_LIBRARIES
+            ${FFMPEG_swresample_LIBRARY}
+            ${FFMPEG_avformat_LIBRARY}
+            ${FFMPEG_avcodec_LIBRARY}
+            ${FFMPEG_avutil_LIBRARY}
+        )
     else()
         message(WARNING "FFMPEG libraries are not a part of AudioCodecs yet. Using any available from the system.")
         set(FFMPEG_INCLUDE_DIRS "" CACHE PATH)
@@ -22,7 +28,7 @@ if(USE_FFMPEG AND MIXERX_LGPL)
         set(FFMPEG_avformat_LIBRARY avformat CACHE PATH)
         set(FFMPEG_avcodec_LIBRARY avcodec CACHE PATH)
         set(FFMPEG_avutil_LIBRARY avutil CACHE PATH)
-        set(FFMPEG_LIBRARIES
+        set(FFMPEG_LINK_LIBRARIES
             ${FFMPEG_swresample_LIBRARY}
             ${FFMPEG_avformat_LIBRARY}
             ${FFMPEG_avcodec_LIBRARY}
@@ -42,17 +48,18 @@ if(USE_FFMPEG AND MIXERX_LGPL)
         message("== using FFMPEG (LGPL v2.1) ==")
         list(APPEND SDL_MIXER_DEFINITIONS -DMUSIC_FFMPEG)
         if(NOT USE_SYSTEM_AUDIO_LIBRARIES OR NOT USE_FFMPEG_DYNAMIC)
-            list(APPEND SDLMixerX_LINK_LIBS ${FFMPEG_LIBRARIES})
+            list(APPEND SDLMixerX_LINK_LIBS ${FFMPEG_LINK_LIBRARIES})
         endif()
         list(APPEND SDL_MIXER_INCLUDE_PATHS ${FFMPEG_INCLUDE_DIRS})
         list(APPEND SDLMixerX_SOURCES
             ${CMAKE_CURRENT_LIST_DIR}/music_ffmpeg.c
             ${CMAKE_CURRENT_LIST_DIR}/music_ffmpeg.h
         )
-        set_source_files_properties("${CMAKE_CURRENT_LIST_DIR}/music_ffmpeg.c"
-            COMPILE_FLAGS
-                "-std=c99"
-        )
+        if(DEFINED FLAG_C99)
+            set_source_files_properties("${CMAKE_CURRENT_LIST_DIR}/music_ffmpeg.c"
+                COMPILE_FLAGS ${FLAG_C99}
+            )
+        endif()
         appendPcmFormats("WMA;AAC;OPUS")
     else()
         message("-- skipping FFMPEG --")
