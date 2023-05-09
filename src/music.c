ca91baba4a2230cf3c5c0d8fed65503e088a3b92
Wohlstand <admin@wohlnet.ru>
2023-05-09 06:04:04 +0300

    pxtone: Added inital usage

--- src/music.c
+++ src/music.c
@@ -46,6 +46,7 @@
 #include "music_midi_opn.h"
 #include "music_midi_edmidi.h"
 #include "music_ffmpeg.h"
+#include "music_pxtone.h"
 
 #include "utils.h"
 
@@ -605,6 +606,9 @@ static Mix_MusicInterface *s_music_interfaces[] =
 #endif
 #ifdef MUSIC_FFMPEG
     &Mix_MusicInterface_FFMPEG,
+#endif
+#ifdef MUSIC_PXTONE
+    &Mix_MusicInterface_PXTONE,
 #endif
 #ifdef MUSIC_MOD_XMP
     &Mix_MusicInterface_XMP,
@@ -1605,6 +1609,11 @@ Mix_MusicType detect_music_type(SDL_RWops *src)
         return MUS_FFMPEG;
     if (SDL_memcmp(magic + 4, "ftypM4A ", 8) == 0) /* AAC */
         return MUS_FFMPEG;
+#endif
+
+#ifdef MUSIC_PXTONE
+    if (SDL_memcmp(magic, "PTTUNE ", 6) == 0)
+        return MUS_PXTONE;
 #endif
 
 #if defined(MUSIC_MP3_MPG123) || defined(MUSIC_MP3_DRMP3)
