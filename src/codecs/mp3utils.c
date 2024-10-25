182cfd2d45d537837bcc0abd22d57ac9863b5a41
Wohlstand <admin@wohlnet.ru>
2024-10-25 13:34:05 +0300

    mp3utils.c: Use the ID3v2_FIELD_TAG_LENGTH instead of raw number

--- src/codecs/mp3utils.c
+++ src/codecs/mp3utils.c
@@ -231,7 +231,7 @@ static SDL_INLINE Sint32 id3v2_synchsafe_decode(const Uint8 *data)
 static long get_id3v2_len(const Uint8 *data, long length)
 {
     /* size is a 'synchsafe' integer (see above) */
-    long size = id3v2_synchsafe_decode(data + 6);
+    long size = id3v2_synchsafe_decode(data + ID3v2_FIELD_TAG_LENGTH);
     size += ID3v2_HEADER_SIZE; /* header size */
     /* ID3v2 header[5] is flags (bits 4-7 only, 0-3 are zero).
      * bit 4 set: footer is present (a copy of the header but
