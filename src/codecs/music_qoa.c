0ac528cb219949d77d1379be2e6ff08ecc71643d
Wohlstand <admin@wohlnet.ru>
2025-06-27 09:50:43 +0300

    music_qoa.c: Ported most of music_ogg.c's features
    like speed and multichannel

--- src/codecs/music_qoa.c
+++ src/codecs/music_qoa.c
@@ -32,6 +32,20 @@
 #undef inline
 
 
+/* Global flags which are applying on initializing of Vorbis player with a file */
+typedef struct {
+    double speed;
+} QOAVorbis_Setup;
+
+static QOAVorbis_Setup qoa_setup = {
+    1.0
+};
+
+static void QOAVorbis_SetDefault(QOAVorbis_Setup *setup)
+{
+    setup->speed = 1.0;
+}
+
 typedef struct
 {
     int volume;
@@ -67,6 +81,9 @@ typedef struct
     Uint32 loop_end;
     Uint32 loop_len;
 
+    int computed_src_rate;
+    double speed;
+
     SDL_bool multitrack;
     Uint32 multitrack_mute[256];
     Uint32 multitrack_channels;
@@ -79,6 +96,30 @@ typedef struct
 static int QOA_Seek(void *ctx, double pos);
 static void QOA_Delete(void *ctx);
 static void QOA_CleanUp(SDL_RWops *src, QOA_Music *music);
+
+static int QOA_UpdateSpeed(QOA_Music *music)
+{
+    if (music->computed_src_rate != -1) {
+        return 0;
+    }
+
+    music->computed_src_rate = music->info.samplerate * music->speed;
+    if (music->computed_src_rate < 1000) {
+        music->computed_src_rate = 1000;
+    }
+
+    if (music->stream) {
+        SDL_FreeAudioStream(music->stream);
+    }
+
+    music->stream = SDL_NewAudioStream(AUDIO_S16SYS, (Uint8)(music->multitrack ? music->multitrack_channels : music->info.channels), music->computed_src_rate,
+                                       music_spec.format, music_spec.channels, music_spec.freq);
+    if (!music->stream) {
+        return -1;
+    }
+
+    return 0;
+}
 
 static SDL_bool _XQOA_ReadMetaTag(QOA_Music *music, const char *tag_name, Mix_MusicMetaTag tag_type)
 {
@@ -112,14 +153,72 @@ static SDL_bool _XQOA_ReadMetaTag(QOA_Music *music, const char *tag_name, Mix_Mu
     return SDL_TRUE;
 }
 
+static void process_args(const char *args, QOAVorbis_Setup *setup)
+{
+#define ARG_BUFFER_SIZE    1024
+    char arg[ARG_BUFFER_SIZE];
+    char type = '-';
+    size_t maxlen = 0;
+    size_t i, j = 0;
+    int value_opened = 0;
+    if (args == NULL) {
+        return;
+    }
+    maxlen = SDL_strlen(args);
+    if (maxlen == 0) {
+        return;
+    }
+
+    maxlen += 1;
+    QOAVorbis_SetDefault(setup);
+
+    for (i = 0; i < maxlen; i++) {
+        char c = args[i];
+        if (value_opened == 1) {
+            if ((c == ';') || (c == '\0')) {
+                /* int value = 0; */
+                arg[j] = '\0';
+                /* value = SDL_atoi(arg); */
+                switch(type)
+                {
+                case 's':
+                    if (arg[0] == '=') {
+                        setup->speed = SDL_strtod(arg + 1, NULL);
+                        if (setup->speed < 0.0) {
+                            setup->speed = 1.0;
+                        }
+                    }
+                    break;
+                case '\0':
+                    break;
+                default:
+                    break;
+                }
+                value_opened = 0;
+            }
+            arg[j++] = c;
+        } else {
+            if (c == '\0') {
+                return;
+            }
+            type = c;
+            value_opened = 1;
+            j = 0;
+        }
+    }
+#undef ARG_BUFFER_SIZE
+}
+
 /* Load a libxmp stream from an SDL_RWops object */
-void *QOA_CreateFromRW(SDL_RWops *src, int freesrc)
+void *QOA_CreateFromRWex(SDL_RWops *src, int freesrc, const char *args)
 {
     QOA_Music *music;
     Uint8 header[QOA_MIN_FILESIZE];
     Uint8 read_buf[4];
     Uint32 xqoa_head_size;
     Uint32 xqoa_data_size;
+    Uint8 in_channels;
+    QOAVorbis_Setup setup = qoa_setup;
 
     music = (QOA_Music *)SDL_calloc(1, sizeof(*music));
     if (!music) {
@@ -130,6 +229,11 @@ void *QOA_CreateFromRW(SDL_RWops *src, int freesrc)
     music->src = src;
     music->xqoa_data_offset = 0;
     music->xqoa_data_size = SDL_RWsize(src);
+    music->volume = MIX_MAX_VOLUME;
+
+    process_args(args, &setup);
+
+    music->speed = setup.speed;
 
     if (SDL_RWread(src, header, 1, QOA_MIN_FILESIZE) != QOA_MIN_FILESIZE) {
         Mix_SetError("QOA: Can't read header.");
@@ -248,7 +352,12 @@ void *QOA_CreateFromRW(SDL_RWops *src, int freesrc)
         music->first_frame_pos += music->xqoa_data_offset;
     }
 
-    music->volume = MIX_MAX_VOLUME;
+    if (music->info.channels == 0 || music->info.channels > 0xFF) {
+        Mix_SetError("QOA: Invalid number of channels.");
+        QOA_CleanUp(src, music);
+        return NULL;
+    }
+
     music->num_channels = music->info.channels;
 
     SDL_RWseek(src, music->first_frame_pos, SEEK_SET);
@@ -280,7 +389,27 @@ void *QOA_CreateFromRW(SDL_RWops *src, int freesrc)
         return NULL;
     }
 
-    music->stream = SDL_NewAudioStream(AUDIO_S16SYS, music->info.channels, music->info.samplerate,
+    music->computed_src_rate = music->info.samplerate * music->speed;
+    if (music->computed_src_rate < 1000) {
+        music->computed_src_rate = 1000;
+    }
+
+    if (music->multitrack_channels > 0 && music->multitrack_tracks > 0) {
+        music->multitrack = SDL_TRUE;
+        if (music->multitrack_channels * music->multitrack_tracks > music->info.channels) {
+            Mix_SetError("XQOA: Invalid multitrack setup: product of channels and tracks must not be bigger than actual channels number at this file.");
+            QOA_CleanUp(src, music);
+            return NULL;
+        }
+    }
+
+    if (music->multitrack) {
+        in_channels = (Uint8)music->multitrack_channels;
+    } else {
+        in_channels = (Uint8)music->info.channels;
+    }
+
+    music->stream = SDL_NewAudioStream(AUDIO_S16SYS, in_channels, music->computed_src_rate,
                                        music_spec.format, music_spec.channels, music_spec.freq);
     if (!music->stream) {
         Mix_SetError("QOA: Can't initialize stream.");
@@ -295,6 +424,11 @@ void *QOA_CreateFromRW(SDL_RWops *src, int freesrc)
 
     music->freesrc = freesrc;
     return music;
+}
+
+static void *QOA_CreateFromRW(struct SDL_RWops *src, int freesrc)
+{
+    return QOA_CreateFromRWex(src, freesrc, NULL);
 }
 
 /* Set the volume for a libxmp stream */
@@ -439,11 +573,14 @@ static int QOA_SeekToSample(QOA_Music *music, Uint32 dst_pos)
 static int QOA_GetSome(void *context, void *data, int bytes, SDL_bool *done)
 {
     QOA_Music *music = (QOA_Music *)context;
-    SDL_bool looped = SDL_FALSE;
-    int filled, amount, channels, result;
+    SDL_bool looped = SDL_FALSE, retry_get = SDL_FALSE;
+    int filled, amount, channels, result, amount_samples, div_chans, i;
     int frame_size = (sizeof(Sint16) * music->num_channels);
-    Uint32 pcmPos;
+    Uint32 pcmPos, j, k;
+    Sint16 buf_mid[8];
+    Sint16 *buf_in, *buf_out;
 
+try_get:
     filled = SDL_AudioStreamGet(music->stream, data, bytes);
     if (filled != 0) {
         return filled;
@@ -455,11 +592,52 @@ static int QOA_GetSome(void *context, void *data, int bytes, SDL_bool *done)
         return 0;
     }
 
+    if (music->computed_src_rate < 0) {
+        result = QOA_UpdateSpeed(music);
+        if (result < 0) {
+            return -1; /* Error has occured */
+        }
+        retry_get = SDL_TRUE;
+    }
+
     amount = _QOA_ReadSamples(music, (Sint16*)music->buffer, music->buffer_size / frame_size);
     amount *= frame_size;
 
     channels = music->info.channels;
     pcmPos = music->sample_pos;
+
+    if (music->multitrack && amount > 0) { /* Mix channels into desired output */
+        amount_samples = amount / frame_size;
+        amount = music->multitrack_channels * amount_samples * sizeof(Sint16);
+        channels = music->multitrack_channels;
+        div_chans = (music->info.channels / music->multitrack_channels);
+        buf_in = (Sint16*)music->buffer;
+        buf_out = (Sint16*)music->buffer;
+
+        for (i = 0; i < amount_samples; ++i) {
+            for (k = 0; k < music->multitrack_channels; ++k) {
+                buf_mid[k] = 0;
+            }
+
+            for (j = 0; j < music->multitrack_tracks; ++j) {
+                if (music->multitrack_mute[j]) {
+                    continue;
+                }
+
+                for (k = 0; k < music->multitrack_channels; ++k) {
+                    buf_mid[k] += buf_in[(j * music->multitrack_channels) + k] / div_chans;
+                }
+            }
+
+            for (k = 0; k < music->multitrack_channels; ++k) {
+                buf_out[k] = buf_mid[k];
+            }
+
+            buf_in += music->info.channels;
+            buf_out += music->multitrack_channels;
+        }
+
+    }
 
     if (music->loop && (music->play_count != 1) && (pcmPos >= music->loop_end)) {
         amount -= (int)((pcmPos - music->loop_end) * channels) * (int)sizeof(Sint16);
@@ -493,6 +671,10 @@ static int QOA_GetSome(void *context, void *data, int bytes, SDL_bool *done)
                 return -1;
             }
         }
+    }
+
+    if (retry_get) {
+        goto try_get; /* Prevent the false-positive "too many zero loop cycles" break condition match */
     }
 
     return 0;
@@ -530,6 +712,23 @@ static double QOA_Duration(void *context)
     return (double)music->info.samples / (double)music->info.samplerate;
 }
 
+static int QOA_SetSpeed(void *context, double speed)
+{
+    QOA_Music *music = (QOA_Music *)context;
+    if (speed <= 0.01) {
+        speed = 0.01;
+    }
+    music->speed = speed;
+    music->computed_src_rate = -1;
+    return 0;
+}
+
+static double QOA_GetSpeed(void *context)
+{
+    QOA_Music *music = (QOA_Music *)context;
+    return music->speed;
+}
+
 static double QOA_LoopStart(void *music_p)
 {
     QOA_Music *music = (QOA_Music *)music_p;
@@ -555,6 +754,25 @@ static double QOA_LoopLength(void *music_p)
         return (double)music->loop_len / music->info.samplerate;
     }
     return -1.0;
+}
+
+static int QOA_GetTracksCount(void *music_p)
+{
+    QOA_Music *music = (QOA_Music *)music_p;
+    if (music->multitrack) {
+        return music->multitrack_tracks;
+    }
+    return -1;
+}
+
+static int QOA_SetTrackMute(void *music_p, int track, int mute)
+{
+    QOA_Music *music = (QOA_Music *)music_p;
+    if (music->multitrack && track >= 0 && (Uint32)track < music->multitrack_tracks) {
+        music->multitrack_mute[track] = mute;
+        return 0;
+    }
+    return -1;
 }
 
 static const char* QOA_GetMetaTag(void *context, Mix_MusicMetaTag tag_type)
@@ -611,7 +829,7 @@ Mix_MusicInterface Mix_MusicInterface_QOA =
     NULL,   /* Load */
     NULL,   /* Open */
     QOA_CreateFromRW,
-    NULL,   /* CreateFromRWex [MIXER-X]*/
+    QOA_CreateFromRWex,
     NULL,   /* CreateFromFile */
     NULL,   /* CreateFromFileEx [MIXER-X]*/
     QOA_SetVolume,
@@ -627,12 +845,12 @@ Mix_MusicInterface Mix_MusicInterface_QOA =
     QOA_Duration,
     NULL,   /* SetTempo [MIXER-X] */
     NULL,   /* GetTemp [MIXER-X] */
-    NULL,   /* SetSpeed [MIXER-X] */
-    NULL,   /* GetSpeed [MIXER-X] */
+    QOA_SetSpeed,
+    QOA_GetSpeed,
     NULL,   /* SetPitch [MIXER-X] */
     NULL,   /* GetPitch [MIXER-X] */
-    NULL,   /* GetTracksCount [MIXER-X] */
-    NULL,   /* SetTrackMute [MIXER-X] */
+    QOA_GetTracksCount,
+    QOA_SetTrackMute,
     QOA_LoopStart,
     QOA_LoopEnd,
     QOA_LoopLength,
